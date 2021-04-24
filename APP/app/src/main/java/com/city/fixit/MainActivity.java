a4915ab8f2acc7204c38688d72141f6eb7084f8b
BrunoMarini <bgmarini@hotmail.com>
2021-04-24 10:39:42 -0300

    Empty project

---
+++ APP/app/src/main/java/com/city/fixit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.city.fixit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
